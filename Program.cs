768fb0e819ccf275bb81781b90d6665066219f96
skyler dennis <skylerdenniss11@gmail.com>
2024-08-29 11:20:01 -0400

    I declared, Initialized and Interpolated the variables

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string Name = "Skyler";
+int Age = 21;
+char LastNameInitial = 'D';
+bool Dreamjob = true;
+double completedamount = 15.5;
+decimal goalcompleted = 100.00m;
+
+Console.WriteLine ($"Hello my name is {Name}.{LastNameInitial}");
+ Console.WriteLine ($"I am {Age} years old");
+Console.WriteLine ($"I have always wanted to be a software engenier, So I hope it comes {Dreamjob}");
+Console.WriteLine($"I am curently {completedamount} Done with my bootcamp which is exiting but my goal is {goalcompleted}");
